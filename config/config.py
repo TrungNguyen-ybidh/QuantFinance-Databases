293eb613882ff48eee17bbb154ed1e5f37c33bfd
TrungNguyen-ybidh <you@example.com>
2026-03-29 17:21:57 -0400

    update sql

--- config/config.py
+++ config/config.py
@@ -1,3 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://root:Aatroxkalistartop123@localhost:3306/finance_db")
+engine = create_engine(
+    "mysql+pymysql://tnguyen:thyngu123@192.168.1.26:3306/finance_db"
+)
+
+
+with engine.connect() as conn:
+    result = conn.execute(text("SHOW TABLES;"))
+    for row in result:
+        print(row)
