16d6aaab085eaf97fcd31674eb2e8409457671e9
TrungNguyen-ybidh <you@example.com>
2026-04-01 21:37:34 -0400

    cleaning and uploading price_daily

--- config/config.py
+++ config/config.py
@@ -7,6 +7,3 @@ engine = create_engine(
     "mysql+pymysql://tnguyen:thyngu123@10.10.73.34:3306/finance_db"
 )
 
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT 1"))
-    print(result.fetchone())
